715f56b04350e2a1172cd84334d1796117431ae8
Arekinex <151863116+Arekinex@users.noreply.github.com>
2024-01-21 20:17:03 +0100

    Base
    :D

---
+++ GaussLinearMethod/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace GaussLinearMethod
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView4 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 132);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(265, 389);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(283, 132);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(218, 389);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(535, 132);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowHeadersWidth = 51;
+            this.dataGridView3.RowTemplate.Height = 24;
+            this.dataGridView3.Size = new System.Drawing.Size(145, 389);
+            this.dataGridView3.TabIndex = 2;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(59, 64);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "N = ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(202, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "GenerateNumbers";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(202, 42);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(114, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "SolveEquation";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(202, 72);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Test";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView4
+            // 
+            this.dataGridView4.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView4.Location = new System.Drawing.Point(686, 132);
+            this.dataGridView4.Name = "dataGridView4";
+            this.dataGridView4.RowHeadersWidth = 51;
+            this.dataGridView4.RowTemplate.Height = 24;
+            this.dataGridView4.Size = new System.Drawing.Size(159, 389);
+            this.dataGridView4.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(683, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 16);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Sum Of A";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 16);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "A Table";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(287, 112);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 16);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "X Table";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(532, 112);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 16);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "B Table";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(355, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(185, 23);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "GenerateComplexNumbers";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(355, 41);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(185, 23);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "SolveComplexEqaution";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(557, 12);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(188, 23);
+            this.button6.TabIndex = 15;
+            this.button6.Text = "GenerateHigherDimensions";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(557, 41);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(188, 23);
+            this.button7.TabIndex = 16;
+            this.button7.Text = "SolveHigherDEquations";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(557, 71);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(188, 23);
+            this.button8.TabIndex = 17;
+            this.button8.Text = "TestHigherDEquations";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(23, 38);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(31, 16);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "M = ";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(59, 36);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown2.TabIndex = 19;
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(889, 594);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+    }
+}
+
---
+++ GaussLinearMethod/Form1.cs
@@ -0,0 +1,297 @@
+﻿using BibKlas.AlgebraLiniowa;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlTypes;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Numerics;
+
+namespace GaussLinearMethod
+{
+    public partial class Form1 : Form
+    {
+        int N;
+        int M;
+        double[,] A;
+        double[] B, X, Test;
+        double[,] BB, XX, TEST_X;
+        bool IsGenerated = false;
+        Complex[,] cA;
+        Complex[] cB, cX;
+        
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {     
+            //N varible set
+            N = ( int )numericUpDown1.Value;
+            if ( N != 0 )
+            {
+                SetGrid();
+            }                    
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Generate
+            Random random = new Random();
+            double Ax;
+            double suma = 0;           
+            N = ( int )numericUpDown1.Value;
+            SetGrid();
+            A = new double[ N + 1, N + 1 ];
+            B = new double[ N + 1 ];
+            X = new double[ N + 1 ];
+            Test = new double[ N + 1 ];
+
+            for (int i = 1; i <= N; i++)
+            {
+                suma = 0;
+                for (int j = 1; j <= N; j++)
+                {
+                    Ax = random.Next(1, 100);
+                    dataGridView1[i - 1, j - 1].Value = Ax.ToString();
+                    A[i, j] = Ax;
+                    suma = suma + Ax;
+                    
+                }
+                Test[i] = suma;
+                dataGridView4[0, i - 1].Value = suma.ToString();
+                Ax = random.Next(1, 100);
+                B[i] = Ax;
+                dataGridView3[0, i - 1].Value = Ax;
+            }
+            IsGenerated = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Test Button
+            if (IsGenerated)
+            { 
+                if (N > 1)
+                {
+                    MetodaGaussa.RozRowMacGaussa(A, Test, X, 2e-13);
+                    ShowColumn(X, dataGridView2);
+                }
+                else
+                {
+                    MessageBox.Show("N is less than 2");
+                }
+                IsGenerated = false;
+            }
+            else
+            {
+                MessageBox.Show("Please generate new set");
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Solve Button         
+            if (IsGenerated)
+            {
+                if (N > 1)
+                {
+                    MetodaGaussa.RozRowMacGaussa(A, B, X, 2e-13);
+                    ShowColumn(X, dataGridView2);
+                }
+                else
+                {
+                    MessageBox.Show("N is less than 2");
+                }
+                IsGenerated = false;
+            }
+            else
+            {
+                MessageBox.Show("Please generate new set");
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //Genrate higher dimensions
+            Random random = new Random();
+            double Ax;
+            double suma = 0;
+            N = (int)numericUpDown1.Value;
+            SetGridForMDimentions();
+            A = new double[N + 1, N + 1];
+            BB = new double[N + 1, M + 1];
+            XX = new double[N + 1, M + 1];
+            Test = new double[N + 1];
+
+            for (int i = 1; i <= N; i++)
+            {
+                suma = 0;
+                for (int j = 1; j <= N; j++)
+                {                   
+                    Ax = random.Next(1, 100);
+                    dataGridView1[i - 1, j - 1].Value = Ax.ToString();
+                    A[i, j] = Ax;
+                    if(i == 1)
+                    {
+                        dataGridView1[i - 1, j - 1].Value = "uuu";
+                    }
+                    suma += Ax;
+                }              
+                Test[i] = suma;
+                dataGridView4[0, i - 1].Value = suma.ToString();                             
+            }
+           
+
+            for (int i = 1; i <= N; i++)
+            {
+                
+                for (int k = 1; k <= M; k++)
+                {
+                    Ax = random.Next(1, 100);
+                    dataGridView3[i - 1, k - 1].Value = Ax.ToString();
+                    BB[i, k] = Ax;
+                }
+                Test[i] = suma;
+                dataGridView4[0, i - 1].Value = suma;
+            }
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            //Solve higher dimensions
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            //Test higher dimensions
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            //M variable set
+            M = (int)numericUpDown2.Value;
+            SetGridForMDimentions();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //Solve Complex
+            MetodaGaussa.RozRowMacGaussa(cA, cB, cX, 2e-13);
+            for(int i = 1; i <= N; i++)
+            {
+                dataGridView2[0,i - 1].Value = cX[i].Real.ToString("F3") + "," + cX[i].Imaginary.ToString("F3") + "i";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //Generate Complex            
+            Random random = new Random();
+            double Ax;
+            double Bx;          
+            N = (int)numericUpDown1.Value;
+            SetGrid();
+            cA = new Complex[N + 1, N + 1];           
+            cB = new Complex[N + 1];
+            cX = new Complex[N + 1];
+                      
+            for (int i = 1; i <= N; i++)
+            {               
+                for (int j = 1; j <= N; j++)
+                {                                       
+                    Ax = random.Next(1, 100);
+                    Bx = random.Next(1, 100);
+                    cA[i,j] = new Complex(Ax, Bx);
+                    dataGridView1[i - 1, j - 1].Value = Ax.ToString() +","+ Bx.ToString() +"i";
+                }
+                Ax = random.Next(1, 100);
+                Bx = random.Next(1, 100);
+                cB[i] = new Complex(Ax, Bx);
+                dataGridView3[0,i - 1].Value = Ax.ToString() + "," + Bx.ToString() +"i";                
+            }           
+        }
+
+       
+
+        void SetGrid()
+        {
+            dataGridView1.ColumnCount = N;
+            dataGridView1.RowCount = N;
+            dataGridView2.RowCount = N;
+            dataGridView3.RowCount = N;
+            dataGridView4.RowCount = N;
+            
+            dataGridView1.ColumnHeadersHeight = 50;
+            dataGridView2.ColumnHeadersHeight = 50;
+            dataGridView3.ColumnHeadersHeight = 50;
+            dataGridView4.ColumnHeadersHeight = 50;
+            dataGridView2.Columns[ 0 ].Width = 95;
+            dataGridView3.Columns[ 0 ].Width = 55;
+            dataGridView4.Columns[ 0 ].Width = 65;
+            for( int i = 0; i < N; i++ )
+            {
+                dataGridView1.Columns[ i ].Width = 45;
+                dataGridView1.Columns[ i ].HeaderText = ( i + 1 ).ToString();
+                dataGridView1.Rows[ i ].HeaderCell.Value = ( i + 1 ).ToString();
+                dataGridView2.Rows[ i ].HeaderCell.Value = ( i + 1 ).ToString();
+                dataGridView3.Rows[ i ].HeaderCell.Value = ( i + 1 ).ToString();
+                dataGridView4.Rows[ i ].HeaderCell.Value = ( i + 1 ).ToString();
+
+            }
+        }
+
+        void ShowColumn( double[] _X, DataGridView dataGridView )
+        {
+            for ( int i = 1; i <= N; i++ )
+            {
+                dataGridView[ 0, i - 1 ].Value = _X[ i ];
+            }
+        }
+
+        void SetGridForMDimentions()
+        {
+            dataGridView1.ColumnCount = N;
+            dataGridView2.ColumnCount = M;
+            dataGridView3.ColumnCount = M;
+            
+            dataGridView1.RowCount = N;
+            dataGridView2.RowCount = N;
+            dataGridView3.RowCount = N;
+            dataGridView4.RowCount = N;
+
+            dataGridView1.ColumnHeadersHeight = 50;
+            dataGridView2.ColumnHeadersHeight = 50;
+            dataGridView3.ColumnHeadersHeight = 50;
+            dataGridView4.ColumnHeadersHeight = 50;
+            dataGridView2.Columns[0].Width = 95;
+            dataGridView3.Columns[0].Width = 55;
+            dataGridView4.Columns[0].Width = 65;
+            for (int i = 0; i < N; i++)
+            {
+                dataGridView1.Columns[i].Width = 45;
+                dataGridView1.Columns[i].HeaderText = (i + 1).ToString();
+                
+                
+                dataGridView1.Rows[i].HeaderCell.Value = (i + 1).ToString();
+                dataGridView2.Rows[i].HeaderCell.Value = (i + 1).ToString();
+                dataGridView3.Rows[i].HeaderCell.Value = (i + 1).ToString();
+                dataGridView4.Rows[i].HeaderCell.Value = (i + 1).ToString();
+
+            }
+            for(int i = 0; i < M; i++)
+            {
+                dataGridView2.Columns[i].HeaderText = (i + 1).ToString();
+                dataGridView3.Columns[i].HeaderText = (i + 1).ToString();
+            }
+        }
+    }
+}
